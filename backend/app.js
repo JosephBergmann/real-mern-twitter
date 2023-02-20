b2ed79dc2b074ee403cc7e7b2cdee857a0352f5f
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2023-02-19 20:35:21 -0500

    set up api routes and removed extraneous routes

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const tweetsRouter = require('./routes/api/tweets')
+const usersRouter = require('./routes/api/users');
 
 var app = express();
 
@@ -12,9 +11,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;
